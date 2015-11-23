6fd71a72f7008c8dd4e8e9c0730d4ffda7524d33
Vishnu karthik <vishnukl@usc.edu>
2015-11-23 10:44:58 -0800

    filtering by tags and filter tolerance

--- app/app.js
+++ app/app.js
@@ -1,20 +1,27 @@
 angular.module('app', [])
     .controller('ReadingListCtrl', ['$scope', '$http', function ReadingListCtrl($scope, $http) {
+        $scope.filterTolerance = 10;
+        $scope.numTags = 10;
         $http.get('matches.json').success(function (data) {
             $scope.result = data;
-        })
-    }])
-    .directive('topic', function () {
-        return {
-            restrict: 'E',
-            scope: {
-                data: '=',
-                tags: '=',
-                ft: '='
-            },
-            templateUrl: 'topic.html'
+            $scope.matchedTopic = data['matched-topic'];
+            $scope.dependentTopics = data['dependent-topics'];
+            $scope.filterTag = "All"
+        });
+        $scope.changeFilterTag = function (newTag) {
+            $scope.filterTag = newTag;
         };
-    })
+        $scope.filterBySelectedTag = function (dependentTopic) {
+            if ($scope.filterTag == "All") {
+                return true;
+            }
+            var words = dependentTopic.words.map(function (word) {
+                return word.word;
+            });
+            var foundIndex = words.indexOf($scope.filterTag);
+            return (foundIndex > -1 ) && (foundIndex < $scope.filterTolerance);
+        };
+    }])
     .directive('document', function () {
         return {
             restrict: 'E',
