63b8fe2bbf77c4b5b85f95c9d50cf516ad4a55bd
Vishnu karthik <vishnukl@usc.edu>
2015-12-10 13:07:32 +0530

    rudimentary topic tree

--- app/app.js
+++ app/app.js
@@ -1,26 +1,21 @@
-angular.module('app', [])
+angular.module('app', ['treeControl'])
     .controller('ReadingListCtrl', ['$scope', '$http', function ReadingListCtrl($scope, $http) {
-        $scope.filterTolerance = 10;
-        $scope.numTags = 10;
+
+        $scope.treeOptions = {
+            nodeChildren: "dependentTopics",
+            dirSelectable: true,
+            multiSelection: true
+        };
+
+        $scope.topicSelected = function (node, selected) {
+            console.log(node);
+        };
+
         $http.get('matches.json').success(function (data) {
             $scope.result = data;
-            $scope.matchedTopic = data['matched-topic'];
-            $scope.dependentTopics = data['dependent-topics'];
-            $scope.filterTag = "All"
+            $scope.keyword = data['keyword'];
+            $scope.matchedTopics = data['matchTopics'];
         });
-        $scope.changeFilterTag = function (newTag) {
-            $scope.filterTag = newTag;
-        };
-        $scope.filterBySelectedTag = function (dependentTopic) {
-            if ($scope.filterTag == "All") {
-                return true;
-            }
-            var words = dependentTopic.words.map(function (word) {
-                return word.word;
-            });
-            var foundIndex = words.indexOf($scope.filterTag);
-            return (foundIndex > -1 ) && (foundIndex < $scope.filterTolerance);
-        };
     }])
     .directive('document', function () {
         return {
@@ -30,5 +25,14 @@ angular.module('app', [])
             },
             templateUrl: 'document.html'
         };
+    })
+    .directive('topic', function () {
+        return {
+            restrict: 'E',
+            scope: {
+                data: '=data'
+            },
+            templateUrl: 'topic.html'
+        };
+
     });
-;
