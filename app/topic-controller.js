9b49391e0b539b2c8c57060918a6f0d8bc116047
Vishnu karthik <vishnukl@usc.edu>
2015-12-12 10:37:31 +0530

    adding checkbox

--- app/topic-controller.js
+++ app/topic-controller.js
@@ -1,15 +1,23 @@
 function TopicController($scope) {
     $scope.topicStrengthStyle = function (allTopics, topic) {
         var strength = (255 - topic.value / allTopics[0].value * 255).toFixed(0);
-        var greenStrength = 180;
+        var redStrength = 38;
+        var greenStrength = 166;
+        var blueStrength = 154;
 
         if (!($scope.isSelected() && greenStrength > strength)) {
             greenStrength = strength
         }
-        var topicColor = 'rgb(' + strength + ',' + greenStrength + ',' + strength + ')';
+        if (!($scope.isSelected() && redStrength > strength)) {
+            redStrength = strength
+        }
+        if (!($scope.isSelected() && blueStrength > strength)) {
+            blueStrength = strength
+        }
+        var topicColor = 'rgb(' + redStrength + ',' + greenStrength + ',' + blueStrength + ')';
         return {color: topicColor}
     };
     $scope.isSelected = function () {
         return R.contains($scope.data, $scope.selected)
-    }
+    };
 }
