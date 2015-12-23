e32507eefff3ae1c3aee242706782ef2eca8b557
Vishnu karthik <vishnukl@usc.edu>
2015-12-23 20:22:44 +0530

    adding abstract

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-angular.module('app', ['treeControl'])
+angular.module('app', ['treeControl', 'ui.bootstrap'])
     .controller('ReadingListCtrl', ['$scope', '$http', function ReadingListCtrl($scope, $http) {
         $scope.selectedTopics = [];
         $scope.expandedNodes = [];
--- app/document-controller.js
+++ app/document-controller.js
@@ -1,6 +1,22 @@
-function DocumentController($scope) {
+function DocumentController($scope, $uibModal) {
     $scope.baseUrl = "http://www.aclweb.org/anthology/";
     $scope.getDocumentUrl = function () {
         return $scope.baseUrl + $scope.documentInfo.ID;
-    }
+    };
+
+    $scope.showAbstract = function () {
+        var modalInstance = $uibModal.open({
+            size: 'lg',
+            templateUrl: 'abstract.html',
+            controller: ["$scope", "$uibModalInstance", "document", function ($scope, $modalInstance, document) {
+                $scope.document = document
+            }],
+            resolve: {
+                document: function () {
+                    return $scope.documentInfo;
+                }
+            }
+        });
+
+    };
 }
