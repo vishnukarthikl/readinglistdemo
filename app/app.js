3682d18d4c9108c94ff435b8b86c0066fe94e702
Vishnu karthik <vishnukl@usc.edu>
2016-02-26 19:26:44 -0800

    reposition graph

--- app/app.js
+++ app/app.js
@@ -5,6 +5,7 @@ angular.module('app', ['treeControl', 'ui.bootstrap', 'ui.materialize', 'angucom
         $scope.selectedTerms = [];
         $scope.filteredDocuments = [];
         $scope.hideTopics = true;
+        $scope.hideTGraph = false;
 
         $http.get('topic_terms.json').success(function (data) {
             $scope.topicTerms = R.map(function (term) {
